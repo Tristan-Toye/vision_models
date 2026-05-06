f849053283eb689c2f027d6f8679d3b37ad37863
Tristan Toye <r0849537@ciney.student.cs.kuleuven.be>
2026-05-06 20:27:52 +0200

    remove backup

--- .backup_strip/zombie_detection/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Zombie detection computer vision pipeline for KAZ (runtime + experiments)."""
-
-from zombie_detection.interface import ZombieDetectorPipeline
-from zombie_detection.yolov11n import YOLOv11nPipeline
-
-__all__ = ["ZombieDetectorPipeline", "YOLOv11nPipeline"]
